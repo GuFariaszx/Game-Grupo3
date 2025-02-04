871bddabc84cf5484c58f4751219f954bbea07fe
GUSTAVO DE FARIAS ZACHARIN <56610008809@info.local>
2025-02-04 14:57:31 -0300

    Game Testado

--- Assets/Scripts/PlayerScript1.cs
+++ Assets/Scripts/PlayerScript1.cs
@@ -2,27 +2,42 @@ using TMPro;
 using UnityEditor.VersionControl;
 using UnityEngine;
 
-public class PlayerSCript1 : MonoBehaviour
+/*
+    Este arquivo possui uma versão comentada com detalhes sobre como cada linha funciona. 
+    A versão comentada contém código mais fácil e simples de ler. Este arquivo está minimizado.
+*/
+
+
+/// <summary>
+/// Script principal para o movimento em terceira pessoa do personagem no jogo.
+/// Certifique-se de que o objeto que receberá este script (o jogador) 
+/// tenha a tag Player e o componente Character Controller.
+/// </summary>
+public class PlayerScript1 : MonoBehaviour
 {
 
-    //Velocidade com que o personagem se move.Não é afetado pela gravidade ou pelo pulo.
+    //"Velocidade com que o personagem se move. Não é afetado pela gravidade ou pelo pulo."
     public float velocity = 5f;
-    //Este valor é adicionado à velocidade enquanto o personagem está correndo.
-    public float sprintAdittion = 3.5f;
-    //Tempo que o personagem fica no ar. Quanto maior o valor, mais tempo o personagem flutua antes de cair.
+    //"Este valor é adicionado à velocidade enquanto o personagem está correndo."
+    public float sprintAdittion = 10f;
+    //"Quanto maior o valor, maior será o pulo do personagem."
+    public float jumpForce = 18f;
+    //"Tempo que o personagem fica no ar. Quanto maior o valor, mais tempo o personagem flutua antes de cair."
     public float jumpTime = 0.85f;
-    //Força que puxa o jogador para baixo. Alterar este valor afeta todos os movimentos, pulos e quedas.
+    //"Força que puxa o jogador para baixo. Alterar este valor afeta todos os movimentos, pulos e quedas."
     public float gravity = 9.8f;
 
     float jumpElapsedTime = 0;
 
     // Estados do jogador
+    bool isJumping = false;
     bool isSprinting = false;
     bool isCrouching = false;
 
     // Entradas
     float inputHorizontal;
     float inputVertical;
+    bool inputJump;
     bool inputCrouch;
     bool inputSprint;
 
@@ -33,15 +48,10 @@ public class PlayerSCript1 : MonoBehaviour
     public int collectedObjects;
     public TextMeshProUGUI coinText;
 
-    //spawnpoint
-    public float threshold = -10f;
-
     void Start()
     {
         cc = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
-        cc.detectCollisions = true;
-
 
         // Mensagem informando que o usuário esqueceu de adicionar um animator
         if (animator == null)
@@ -49,15 +59,17 @@ public class PlayerSCript1 : MonoBehaviour
     }
 
 
-
+    // O Update é usado aqui apenas para identificar as teclas pressionadas e ativar animações
     void Update()
     {
 
-        // entradas
+        // Verificação de entradas
         inputHorizontal = Input.GetAxis("Horizontal");
         inputVertical = Input.GetAxis("Vertical");
+        inputJump = Input.GetAxis("Jump") == 1f;
         inputSprint = Input.GetAxis("Fire3") == 1f;
-        inputCrouch = Input.GetKeyDown(KeyCode.LeftControl);
+        // Infelizmente o GetAxis não funciona com GetKeyDown, então as entradas devem ser verificadas individualmente
+        inputCrouch = Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl);
 
         // Verifica se pressionou a tecla de agachar e altera o estado do jogador
         if (inputCrouch)
@@ -73,7 +85,7 @@ public class PlayerSCript1 : MonoBehaviour
             animator.SetBool("crouch", isCrouching);
 
             // Correr
-            float minimumSpeed = 0.9f;
+            float minimumSpeed = 5.0f;
             animator.SetBool("run", cc.velocity.magnitude > minimumSpeed);
 
             // Sprint
@@ -81,8 +93,24 @@ public class PlayerSCript1 : MonoBehaviour
             animator.SetBool("sprint", isSprinting);
 
         }
+
+        // Animação de pulo
+        if (animator != null)
+            animator.SetBool("air", cc.isGrounded == false);
+
+        // Verifica se pode pular ou não
+        if (inputJump && cc.isGrounded)
+        {
+            isJumping = true;
+            // Desabilita agachar ao pular
+            //isCrouching = false; 
+        }
+
+        HeadHittingDetect();
+
         //coletar os objetos
-       
+        coinText.text = collectedObjects.ToString();
+
 
     }
 
@@ -92,18 +120,33 @@ public class PlayerSCript1 : MonoBehaviour
     {
 
         // Aumento de velocidade ao correr ou desaceleração ao agachar
-        float velocityAdittion = 0;
+        float velocityAdittion = 3f;
         if (isSprinting)
             velocityAdittion = sprintAdittion;
         if (isCrouching)
-            velocityAdittion = -(velocity * 0.50f); // -50% de velocidade
+            velocityAdittion = -(velocity * 0.60f); // -50% de velocidade
 
         // Movimento de direção
         float directionX = inputHorizontal * (velocity + velocityAdittion) * Time.deltaTime;
         float directionZ = inputVertical * (velocity + velocityAdittion) * Time.deltaTime;
         float directionY = 0;
 
-      
+        // Tratamento do pulo
+        if (isJumping)
+        {
+
+            // Aplica inércia e suavidade ao subir o pulo
+            // Não é necessário ao descer, pois a gravidade vai puxar gradualmente
+            directionY = Mathf.SmoothStep(jumpForce, jumpForce * 0.30f, jumpElapsedTime / jumpTime) * Time.deltaTime;
+
+            // Cronômetro do pulo
+            jumpElapsedTime += Time.deltaTime;
+            if (jumpElapsedTime >= jumpTime)
+            {
+                isJumping = false;
+                jumpElapsedTime = 0;
+            }
+        }
 
         // Aplica gravidade no eixo Y
         directionY = directionY - gravity * Time.deltaTime;
@@ -132,6 +175,8 @@ public class PlayerSCript1 : MonoBehaviour
         }
 
         // --- Fim da rotação ---
+
+
         Vector3 verticalDirection = Vector3.up * directionY;
         Vector3 horizontalDirection = forward + right;
 
@@ -140,29 +185,27 @@ public class PlayerSCript1 : MonoBehaviour
 
     }
 
+
+    // Esta função faz com que o personagem finalize o pulo se bater a cabeça em algo
+    void HeadHittingDetect()
+    {
+        float headHitDistance = 1.1f;
+        Vector3 ccCenter = transform.TransformPoint(cc.center);
+        float hitCalc = cc.height / 2f * headHitDistance;
+
+        // Descomente essa linha para ver o raio desenhado na cabeça do personagem
+        // Debug.DrawRay(ccCenter, Vector3.up * headHeight, Color.red);
+
+        if (Physics.Raycast(ccCenter, Vector3.up, hitCalc))
+        {
+            jumpElapsedTime = 0;
+            isJumping = false;
+        }
+    }
+
     public void AddObject()
     {
         this.collectedObjects++;
-        
-    }
 
-    public GameObject respawn;
-
-
-
-    private void OnControllerColliderHit(ControllerColliderHit collision)
-    {
-        if (respawn != null)
-        {
-            if (collision.gameObject.CompareTag("Enemy"))
-            {
-                transform.position = respawn.transform.position;
-            }
-        } else
-        {
-            Debug.Log("O objeto respawn não foi associado!");
-        }
-        
     }
 }
-
